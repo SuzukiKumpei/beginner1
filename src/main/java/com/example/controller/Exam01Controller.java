be4514d492ecdaec57adf3f0d58b6fd84e992711
SuzukiKumpei <suzuki.kumpei@gmail.com>
2020-02-04 14:31:45 +0900

    Exam01Controllerの作成完了

---
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex1")
+public class Exam01Controller {
+	
+	@RequestMapping("")
+	
+	public String index() {
+		return "exam01";
+	}
+	
+
+}
